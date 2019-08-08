1cb0922c5435aace817329a983d72ccaeab2e64d
Scharkee <matas2k@gmail.com>
2019-08-08 22:25:27 +0300

    split endpoint further: separate base property

--- lib/parts/providers.ts
+++ lib/parts/providers.ts
@@ -16,7 +16,7 @@ export interface Providers {
 export interface Provider {
   keyNeeded: boolean;
   key: any;
-  endpoint: { single: string; multiple: string };
+  endpoint: { base: string; single: string; multiple: string };
   handler: Function | undefined;
 }
 
@@ -26,8 +26,9 @@ export interface Provider {
 export const providers: Providers = {
   ExchangeRatesAPI: {
     endpoint: {
-      single: "https://api.exchangeratesapi.io/latest?base=%FROM%&symbols=%TO%",
-      multiple: "https://api.exchangeratesapi.io/latest?base=%FROM%"
+      base: "https://api.exchangeratesapi.io/latest",
+      single: "?base=%FROM%&symbols=%TO%",
+      multiple: "?base=%FROM%"
     },
     keyNeeded: false,
     key: null,
@@ -35,34 +36,44 @@ export const providers: Providers = {
       return data.rates;
     }
   },
-  OpenExchangeRates: {
-    endpoint: { single: "", multiple: "" },
+  CurrencyLayer: {
+    endpoint: {
+      base: "https://apilayer.net/api/live?access_key=%KEY%",
+      single: "&source=%FROM%",
+      multiple: "&source=%FROM%&currencies=%TO%"
+    },
     keyNeeded: true,
     key: undefined,
-    handler: undefined
+    handler: function(data) {
+      let map = {};
+      Object.keys(data.quotes).map(key => {
+        map[key.slice(3)] = data.quotes[key];
+      });
+      return map;
+    }
+  },
+  OpenExchangeRates: {
+    endpoint: {
+      base: "https://openexchangerates.org/api/latest.json?app_id=%KEY%",
+      single: "&base=%FROM%",
+      multiple: "&base=%FROM%"
+    },
+    keyNeeded: true,
+    key: undefined,
+    handler: function(data) {
+      return data.rates;
+    }
   },
   Fixer: {
-    endpoint: { single: "", multiple: "" },
+    endpoint: {
+      base: "https://data.fixer.io/api/latest?access_key=%KEY%",
+      single: "&base=%FROM%&symbols=%TO%",
+      multiple: "&base=%FROM%"
+    },
     keyNeeded: true,
     key: undefined,
-    handler: undefined
-  },
-  IBAN: {
-    endpoint: { single: "", multiple: "" },
-    keyNeeded: true,
-    key: undefined,
-    handler: undefined
-  },
-  CurrencyLayer: {
-    endpoint: { single: "", multiple: "" },
-    keyNeeded: true,
-    key: undefined,
-    handler: undefined
-  },
-  Happi: {
-    endpoint: { single: "", multiple: "" },
-    keyNeeded: true,
-    key: undefined,
-    handler: undefined
+    handler: function(data) {
+      return data.rates;
+    }
   }
 };
