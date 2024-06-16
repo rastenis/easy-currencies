8b09870c4ef471ea03b755dc6ed39cd524ef17bf
Matas Rastenis <matas2k@gmail.com>
2024-06-16 14:41:39 -0700

    Unset failing tests

--- test/providers.errors.test.js
+++ test/providers.errors.test.js
@@ -4,22 +4,19 @@ const to = require("await-to-js").default;
 /**
  *  CurrencyLayer and Fixer api key level failures
  */
-test.failing(
-  "Fails because of insufficient key level (CurrencyLayer)",
-  async () => {
-    const converter = new Converter(
-      "CurrencyLayer",
-      process.env.CURRENCY_LAYER_KEY
-    );
-    // removing default fallback provider
-    converter.remove(converter.active[1]);
-    const [err, value] = await to(converter.convert(15, "CNY", "EUR"));
+test("Fails because of insufficient key level (CurrencyLayer)", async () => {
+  const converter = new Converter(
+    "CurrencyLayer",
+    process.env.CURRENCY_LAYER_KEY
+  );
+  // removing default fallback provider
+  converter.remove(converter.active[1]);
+  const [err, value] = await to(converter.convert(15, "CNY", "EUR"));
 
-    expect(err).toBeTruthy();
-  }
-);
+  expect(err).toBeTruthy();
+});
 
-test.failing("Fails because of insufficient key level (Fixer)", async () => {
+test("Fails because of insufficient key level (Fixer)", async () => {
   const converter = new Converter("Fixer", process.env.FIXER_KEY);
   // removing default fallback provider
   converter.remove(converter.active[1]);
@@ -30,7 +27,7 @@ test.failing("Fails because of insufficient key level (Fixer)", async () => {
 /**
  * Invalid api key failures
  */
-test.failing("Fails because of invalid key (CurrencyLayer)", async () => {
+test("Fails because of invalid key (CurrencyLayer)", async () => {
   const converter = new Converter("CurrencyLayer", "invalid");
   // removing default fallback provider
   converter.remove(converter.active[1]);
@@ -39,7 +36,7 @@ test.failing("Fails because of invalid key (CurrencyLayer)", async () => {
   expect(err).toBeTruthy();
 });
 
-test.failing("Fails because of invalid key (Fixer)", async () => {
+test("Fails because of invalid key (Fixer)", async () => {
   const converter = new Converter("Fixer", "invalid");
   // removing default fallback provider
   converter.remove(converter.active[1]);
@@ -48,7 +45,7 @@ test.failing("Fails because of invalid key (Fixer)", async () => {
   expect(err).toBeTruthy();
 });
 
-test.failing("Fails because of invalid key (OpenExchangeRates)", async () => {
+test("Fails because of invalid key (OpenExchangeRates)", async () => {
   const converter = new Converter("OpenExchangeRates", "invalid");
   // removing default fallback provider
   converter.remove(converter.active[1]);
